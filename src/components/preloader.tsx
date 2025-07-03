06f98c25245ff0132eea8c158ae3bdc8f1122da2
Idighekere Udeme Udo <101113870+Idighekere@users.noreply.github.com>
2025-07-03 02:05:42 +0100

    fix: increase z-index for preloader

--- src/components/preloader.tsx
+++ src/components/preloader.tsx
@@ -14,7 +14,7 @@ export default function PreLoader() {
   if (!loading) return null;
 
   return (
-    <div className="fixed inset-0 bg-background h-screen w-full flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-background h-screen w-full flex items-center justify-center z-[9999]">
       <div className="animate-pulse">
         <Image src="/ethral.svg" alt="Logo" width={120} height={120} className="animate-fade" />
       </div>
